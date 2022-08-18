20f11dae631498328b9daf1ecd6246e1fa1995dc
necelentano <necelentano@gmail.com>
2022-08-18 12:40:41 +0300

    Sidebar Component & next configuration

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,27 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  HashtagIcon,
+  BookmarkIcon,
+  CollectionIcon,
+  DotsCircleHorizontalIcon,
+  MailIcon,
+  UserIcon,
+  HomeIcon,
+} from "@heroicons/react";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div className="relative h-10 w-10">
+        <Image
+          src="https://links.papareact.com/drq"
+          alt="Twitter logo"
+          layout="fill"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
