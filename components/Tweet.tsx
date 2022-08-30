3e04069876cfce449b2c5677919f6bad346e4c8d
necelentano <necelentano@gmail.com>
2022-08-31 01:40:18 +0300

    Tweet Component

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,72 @@
-const Tweet = () => {
-  return <div>Tweet</div>;
+import Image from "next/image";
+import TimeAgo from "react-timeago";
+import { Tweet } from "../types";
+
+import {
+  ChatAlt2Icon,
+  HeartIcon,
+  SwitchHorizontalIcon,
+  UploadIcon,
+} from "@heroicons/react/outline";
+
+interface Props {
+  tweet: Tweet;
+}
+
+const Tweet = ({ tweet }: Props) => {
+  return (
+    <div className="flex flex-col space-x-3 border-y border-gray-100 p-5">
+      <div className="flex space-x-3">
+        <div className="relative h-10 w-10 rounded-full object-cover overflow-hidden">
+          <Image src={tweet.profileImg} alt={tweet.username} layout="fill" />
+        </div>
+
+        <div>
+          <div className="flex items-center space-x-1">
+            <p className="mr-1 font-bold">{tweet.username}</p>
+            <p className="hidden text-sm text-gray-500 sm:inline">
+              @{tweet.username.replace(/\s+/, "").toLocaleLowerCase()}
+            </p>
+
+            <TimeAgo
+              className="text-sm text-gray-500"
+              date={tweet._createdAt}
+            />
+          </div>
+
+          <p className="pt-1">{tweet.text}</p>
+
+          {tweet.image && (
+            <div className="relative h-80 w-full m-5 ml-0 mb-1">
+              <Image
+                src={tweet.image}
+                alt={tweet.username}
+                layout="fill"
+                objectFit="contain"
+                className="rounded-xl"
+              />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className=" mt-5 flex justify-between">
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <ChatAlt2Icon className="h-5 w-5" />
+          <p>5</p>
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <SwitchHorizontalIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <HeartIcon className="h-5 w-5" />
+        </div>
+        <div className="flex cursor-pointer items-center space-x-3 text-gray-400">
+          <UploadIcon className="h-5 w-5" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Tweet;
